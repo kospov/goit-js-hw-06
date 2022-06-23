478bec4995d5b39a709827b4650dfee30faf0643
Konstantin Popov <popov.k.g@gmail.com>
2022-06-23 17:59:03 +0300

    Done Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+const createItemEl = (nameEl) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = nameEl;
+  return itemEl;
+};
+
+ingredients.forEach(ingredient => {
+  listEl.append(createItemEl(ingredient));
+});
+
+console.log(listEl);
+
+
+
+
+
