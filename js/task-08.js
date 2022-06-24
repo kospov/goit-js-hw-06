cd5557f38e9b2d1a8d8a8dcdfabb7f40c2af6501
Konstantin Popov <popov.k.g@gmail.com>
2022-06-24 22:50:31 +0300

    done Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onSubmitEvent);
+
+function onSubmitEvent(event) {
+    event.preventDefault();
+    if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") {
+        alert('Bсі поля повинні бути заповнені.');
+    } else {
+        const userData = {
+            Email: event.currentTarget.elements.email.value,
+            Password: event.currentTarget.elements.password.value,
+        };
+        console.log(userData);
+    };
+    event.currentTarget.elements.email.value = '';
+    event.currentTarget.elements.password.value = '';
+};
+
 
