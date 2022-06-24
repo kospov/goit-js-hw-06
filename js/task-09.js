3e5e856063e446a361be7f8018754d5047ff7fc1
Konstantin Popov <popov.k.g@gmail.com>
2022-06-24 23:20:51 +0300

    done Task  09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+
+
+const bodyEl = document.querySelector('body');
+const textOutputEl = document.querySelector('.color');
+const changeColorBtnEl = document.querySelector('.change-color');
+
+changeColorBtnEl.addEventListener('click', onClickBtnEvent);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+function onClickBtnEvent() {
+  const changeColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = changeColor;
+  textOutputEl.textContent = changeColor;
+};
